d073dbf1ef6406a1f98a986701c4920466ce8b21
harshit <harshitag98.ha@gmail.com>
2022-04-17 11:16:02 +0530

    feedback form

---
+++ Frontend/src/feedback/index.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import FeedBack from "./components/Feedback";
+import "./style.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React"
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <FeedBack
+          style={{ zIndex: "1", position: "fixed", left: "2px!" }}
+          position="left"
+          numberOfStars={5}
+          headerText="Feedback"
+          bodyText="Give your Feedback"
+          buttonText="Feedback"
+          handleClose={() => console.log("handleclose")}
+          handleSubmit={data =>
+            fetch("https://formspree.io/xxxxxxx", {
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+              },
+              method: "POST", // or 'PUT'
+              body: JSON.stringify(data)
+            })
+              .then(response => {
+                if (!response.ok) {
+                  return Promise.reject(
+                    "Our servers are having issues! We couldn't send your feedback!"
+                  );
+                }
+                response.json();
+              })
+              .then(() => {
+                alert("Success!");
+              })
+              .catch(error => {
+                alert(
+                  "Our servers are having issues! We couldn't send your feedback!",
+                  error
+                );
+              })
+          }
+          handleButtonClick={() => console.log("handleButtonClick")}
+        />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
