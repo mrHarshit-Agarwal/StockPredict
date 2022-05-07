c1e64293dade85d574ff302a63c68fd2a3db0bc8
Karan02 <krnptl1234@gmail.com>
2022-05-07 23:20:17 +0530

    asd

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -2,11 +2,11 @@
 import Dashboard from "./views/Dashboard.js";
 import UserProfile from "./views/UserProfile.js";
 import TableList from "./views/TableList.js";
-import Typography from "./views/Typography.js";
-import Icons from "./views/Icons.js";
-import Maps from "./views/Maps.js";
-import Notifications from "./views/Notifications.js";
-import Upgrade from "./views/Upgrade.js";
+// import Typography from "./views/Typography.js";
+// import Icons from "./views/Icons.js";
+// import Maps from "./views/Maps.js";
+// import Notifications from "./views/Notifications.js";
+// import Upgrade from "./views/Upgrade.js";
 import Feedback from "./views/Feedback"
 import Information from "./views/Information.js"
 const dashboardRoutes = [
