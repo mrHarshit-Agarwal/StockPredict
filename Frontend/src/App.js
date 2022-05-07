3aa1866f27016a5b22169830f29ed98b4119edfe
Karan02 <krnptl1234@gmail.com>
2022-05-08 01:35:26 +0530

    dashboard

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
