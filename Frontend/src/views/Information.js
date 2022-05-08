cf6dd11ebdfe79e8d77aa60f358dcff25d89dd8c
harshit <harshitag98.ha@gmail.com>
2022-05-08 12:01:27 +0530

    commit

--- Frontend/src/views/Information.js
+++ Frontend/src/views/Information.js
@@ -27,7 +27,7 @@ const Information = () => {
           <img src={require("../assets/img/LSTM_home.png")} alt="LSTM-image" className="image" width={700} height={312} />
           <br /><br />
           <h4 className="topic-heading">LSTM Model implementation</h4>
-          <img src={require("../assets/img/graph.png")} alt="lstm graph" className="image" width={800} height={400} />
+          <img src={require("../assets/img/graph.png")} alt="lstm graph" className="image" width={815} height={400} />
           <br />
           <h4 className="topic-heading">Recurrent Neural Network (RNN)</h4>
           <p className="para">The neurons of RNN have a cell state/memory, and input is processed according to this internal state, 
