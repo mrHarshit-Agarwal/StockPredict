2798078f08a27d67d4d50020f7bdc7d79c695a56
Honey Patel <you@example.com>
2022-05-11 11:16:06 +0530

    commit

---
+++ Backend/test/feedbackapi.spec.js
@@ -0,0 +1,54 @@
+let mongoose = require("mongoose");
+let Feedback = require('../model/feedback');
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+
+let expect =chai.expect;
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+  describe('/POST feedback ', () => {
+    
+      it('it should not POST feedback request with invalid fields', (done) => {
+        let feedback = {
+          email: "Honeypatel123@gmail.com",
+          name:"honey Patel",
+          desc:"Nice Website"
+          
+        };
+        let size= feedback.email.length;
+       
+        chai.request(server)
+            .post('/api/login')
+            .send(feedback)
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                  //res.body.should.have.property('email').eql('required');
+                  expect(feedback).to.haveOwnProperty('email');
+                  expect(feedback).to.haveOwnProperty('name');
+                  expect(feedback).to.haveOwnProperty('desc');
+                  expect(feedback.email).to.be.a('string');
+                  expect(feedback.name).to.be.a('string');
+                  expect(feedback.desc).to.be.a('string');
+                  expect(feedback).to.have.property('email').with.lengthOf(size);
+                //   expect(feedback).to.have.property('email').that.does.include('@')
+                  expect(feedback).to.have.property('email').equal('Honeypatel123@gmail.com',"valid field");
+                  expect(feedback.desc).to.have.lengthOf.at.least(10);
+                  expect('Content-Type', /json/);
+                //   res.body.errors.pages.should.have.property('kind').eql('required');
+                  
+              done();
+            });
+          
+      });
+
+      
+
+
+
+  
+});
--- Backend/test/userapi.spec.js
+++ Backend/test/userapi.spec.js
@@ -11,12 +11,15 @@ var should = chai.should();
 chai.use(chaiHttp);
 
   describe('/POST user login', () => {
-      it('it should not POST login request without valid fields', (done) => {
+    
+      it('it should not POST login request without invalid fields', (done) => {
         let newUser = {
-          name: "honey Patel",
           email: "Honeypatel123@gmail.com",
+          password:"Honey@123"
           
         };
+        let size= newUser.email.length;
+       
         chai.request(server)
             .post('/api/login')
             .send(newUser)
@@ -25,52 +28,51 @@ chai.use(chaiHttp);
                   res.body.should.be.a('object');
                   //res.body.should.have.property('email').eql('required');
                   expect(newUser).to.haveOwnProperty('email');
+                  expect(newUser).to.haveOwnProperty('password');
                   expect(newUser.email).to.be.a('string');
+                  expect(newUser.password).to.be.a('string');
+                  expect(newUser).to.have.property('email').with.lengthOf(size);
+                  expect(newUser).to.have.property('email').that.does.include('@');
+                  expect(newUser).to.have.property('email').equal('Honeypatel123@gmail.com',"valid field");
+                  expect('Content-Type', /json/);
                 //   res.body.errors.pages.should.have.property('kind').eql('required');
               done();
             });
       });
 
-      it('it should not POST login request without valid datatypes of fields', (done) => {
-        var newUser = {
-         
-          email: "Honeypatel123@gmail.com",
-          password: "Honey@2342"
-          
-        };
-        chai.request(server)
-            .post('/api/login')
-            .send(newUser)
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.should.be.a('object');
-                  newUser.doINeed.suncream(email).should.equal(true);
-                  expect(newUser.email).to.be.a('String');
-                  expect(newUser.password).to.be.a('string');
-                //   res.body.errors.pages.should.have.property('kind').eql('required');
-              done();
-            });
-      });
+      
 
   });
 
   describe('/POST user signup', () => {
+    var newUser = {
+      name: "honey Patel",
+      email: "Honeypatel12378@gmail.com",
+      password: "HoneyPatel@1234"
+    };
+    let size= newUser.name.length;
+    
     it('it should not POST signup request without valid fields', (done) => {
-      var newUser = {
-        name: "honey Patel",
-        email: "Honeypatel12378@gmail.com",
-        password: "HoneyPatel@1234"
-      };
+      
       chai.request(server)
           .post('/api/signup')
           .send(newUser)
           .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                //res.body.should.have.property('email').eql('required');
-                expect(newUser).to.haveOwnProperty('email');
-                expect(newUser.email).to.be.a('string');
-              //   res.body.errors.pages.should.have.property('kind').eql('required');
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            //res.body.should.have.property('email').eql('required');
+            expect(newUser).to.haveOwnProperty('email');
+            expect(newUser).to.haveOwnProperty('password');
+            expect(newUser).to.haveOwnProperty('name');
+            expect(newUser.email).to.be.a('string');
+            expect(newUser.password).to.be.a('string');
+            expect(newUser.name).to.be.a('string');
+            expect(newUser).to.have.property('name').with.lengthOf(size);
+            expect(newUser.name).to.have.lengthOf.at.least(3);
+            expect(newUser.email).contain.oneOf(['@gmail.com', '@yahoo.com']);
+            expect(newUser.password).to.have.lengthOf.at.least(6);
+            expect(newUser).to.have.property('name').equal('honey Patel',"valid field");
+            expect('Content-Type', /json/);
             done();
           });
     });
